5f0651341b440a0b55a2224d75869d52e2286f5b
Ruta Jogani <ruta007jogani@gmail.com>
2026-05-09 18:18:27 +0530

    make new file and add code in home_related_things.py file

---
+++ home_related_things.py
@@ -0,0 +1,22 @@
+def things(): # Home related things 
+
+    things = {
+        "phone": "Here is your phone",
+        "TV": "Turning on the TV",
+        "bag": "Here is your bag",
+        "fan": "Turn on your fan"
+    }
+
+    found = 0
+
+    for thing in things:
+        if thing in text:
+            print("FOUND IT")
+            print(things[thing])
+            engine.say(things[thing])
+            found = 1
+            break
+
+    if found == 0:
+        print("NOT FOUND")
+

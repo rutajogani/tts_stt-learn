de617910425209cce4497d136e8813c26c8d4144
Ruta Jogani <ruta007jogani@gmail.com>
2026-05-21 23:31:03 +0530

    add function in password file

--- password.py
+++ password.py
@@ -1,9 +1,11 @@
 import getpass
 
-password_temp = "hello world"
-final_password = input("Enter Password: ")
 
-if password_temp == final_password:
-    print(final_password)
-else:
-    print("Your Password is not ar")
+def password():
+    password_temp = "hello world"
+    final_password = input("Enter Password: ")
+
+    if password_temp == final_password:
+        print(final_password)
+    else:
+        print("Your Password is not ar")

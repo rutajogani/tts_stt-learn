ae1cc76ceea39201fa55b0f33ef15ad753ca3563
Ruta Jogani <ruta007jogani@gmail.com>
2026-06-11 18:33:53 +0530

    remove space in main.py file

--- main.py
+++ main.py
@@ -5,11 +5,8 @@ from browser import browser
 from home_related_things import things
 from password import password
 
-
 text = listen()
 speak(text)
-
-# password("hello world")
 
 if "data" or "time" in text:
     date_time(text)

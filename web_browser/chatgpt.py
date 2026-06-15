43e567a6966ada6ea05c83822d1c41ab9e97b6e2
Ruta Jogani <ruta007jogani@gmail.com>
2026-06-15 22:59:27 +0530

    add open section of chatgpt file

---
+++ web_browser/chatgpt.py
@@ -0,0 +1,14 @@
+from listen import listen
+from speak import speak
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+
+def chatgpt():
+
+    driver = webdriver.Chrome()
+    print("COMING HERE")
+    driver.get("https://chatgpt.com/")
+    speak("Chatgpt opened")

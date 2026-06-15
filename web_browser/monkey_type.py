c160b9846cf540c7d1f6156e60302787dabc4fde
Ruta Jogani <ruta007jogani@gmail.com>
2026-06-15 22:56:23 +0530

    add open section in monkey type file

--- web_browser/monkey_type.py
+++ web_browser/monkey_type.py
@@ -5,12 +5,10 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
-import requests
-import time
 
+def monkey_type():
 
-def check_open_or_not():
-    r = requests.get("https://www.youtube.com", timeout=5)
-    print("YouTube is reachable:", r.status_code)
-
-driver = None
+    driver = webdriver.Chrome()
+    print("COMING HERE")
+    driver.get("https://monkeytype.com/")
+    speak("Monkey type opened")

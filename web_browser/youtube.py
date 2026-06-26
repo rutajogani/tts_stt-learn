56e68048126e8991bf8753688be59b6147159f27
Ruta Jogani <ruta007jogani@gmail.com>
2026-06-26 23:20:18 +0530

    make little change in youtube file

--- web_browser/youtube.py
+++ web_browser/youtube.py
@@ -32,7 +32,7 @@ def youtube():
             
         # searching in youtube
         elif "search" in command:
-            print("Is ther comeing here?")
+            print("Is there coming here?")
             if driver is None:
                 driver = webdriver.Chrome()
                 hold_web_page("https://youtube.com")
@@ -47,30 +47,6 @@ def youtube():
             search_box.send_keys(what_to_search)
             what_to_search = ""
             search_box.send_keys(Keys.RETURN)
-
-            # try:
-
-            #     if response.status_code == 200:
-            #         search = driver.find_element(
-            #          By.XPATH,
-            #         "//input[@id='search']"
-            #     )
-            #         search.click(what_to_search)    
-            #         speak("searching youtube")
-                    
-            #     else:
-                    # driver = webdriver.Chrome()
-                    # driver.get("https://youtube.com")
-                    
-                #     search = driver.find_element(
-                #     By.XPATH,
-                #     "//input[@id='search']"
-                # )
-                #     search.click(what_to_search)    
-                #     speak("searching youtube")                            
-
-            # except requests.RequestException:
-            #     print("YouTube is not reachable → Do THAT")
 
         #close youtube 
         elif "close" in command:
